eb8d8905304125a23c1f83170eac2f5b0c57db36
HoangAn26 <32327782+HoangAn26@users.noreply.github.com>
2019-08-21 10:17:37 +0700

    Add project files.

---
+++ ACIMApp.Mobile/DownloadReportService.svc.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DevExpress.ExpressApp.ReportsV2.Mobile;
+
+namespace ACIMApp.Mobile {
+    public class DownloadReportService : XafDownloadReportService<ACIMAppMobileApplication> { }
+}
---
+++ ACIMApp.Mobile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ACIMApp.Mobile.Global" Language="C#" %>
